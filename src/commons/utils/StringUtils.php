993e909421ab3754011c54c1030943d5ec4a5e9b
外号猫哥 <cattong@163.com>
2021-09-09 11:57:46 +0800

    更新数据处理

--- src/commons/utils/StringUtils.php
+++ src/commons/utils/StringUtils.php
@@ -3,17 +3,42 @@
 namespace beyong\commons\utils;
 
 class StringUtils {
+	//空字符串判断
+	public static function isEmpty($str)
+	{
+        if (empty($str)) {
+			return true;
+		}
+
+		return false;
+	}
+
+	public static function isBlank($str) 
+	{
+		if (empty($str)) {
+			return true;
+		}
+		if (ctype_space($str)) {
+			return true;
+		}
+		return false;
+	}
+
 	/**
 	 * 生成随机字符串数,数字和小写字母组合
 	 *
 	 */
-	public static function getRandString($length = 32) {
-		$str = '1234567890abcdefghijklmnopqrstuvwxyz';
-	
+	public static function getRandString($length = 32, $chars='1234567890abcdefghijklmnopqrstuvwxyz') 
+	{
+		if (strlen($chars) == 0) {
+            return "";
+		}
+
+		$length = strlen($chars) - 1;
 		$myRand = "";
 		for ($i=0; $i < $length; $i++) {
-			$j = rand(0, 35);
-			$myRand .= $str[$j];
+			$j = rand(0, $length);
+			$myRand .= $chars[$j];
 		}
 	
 		return $myRand;
